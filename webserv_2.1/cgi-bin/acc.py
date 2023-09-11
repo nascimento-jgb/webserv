19edc0f0c286fb54357e5f64f122bd0d94bd4a6c
Joao <nascimento.joaogb@gmail.com>
2023-09-11 14:34:01 +0300

    pyton script modified

--- webserv_2.1/cgi-bin/acc.py
+++ webserv_2.1/cgi-bin/acc.py
@@ -6,12 +6,19 @@ import time
 import hashlib
 import pickle
 import sys
+import os
 
+# -fsanitize=address -g -static-libsan
 class Session:
 	def __init__(self, name):
 		self.name = name
 		self.sid = hashlib.sha1(str(time.time()).encode("utf-8")).hexdigest()
-		with open('cgi-bin/sessions/session_' + self.sid, 'wb') as f:
+		session_dir = 'sessions'
+		os.makedirs(session_dir, exist_ok=True)  # This line creates the directory if it doesn't exist
+		session_file_path = session_dir + '/session_' + self.sid
+		print("Session ID:", self.sid)
+		print("Session File Path:", session_file_path)
+		with open(session_file_path, 'wb') as f:
 			pickle.dump(self, f)
 	def getSid(self):
 		return self.sid
@@ -24,11 +31,11 @@ class UserDataBase:
 	def addUser(self, username, password, firstname):
 		self.user_pass[username] = password
 		self.user_firstname[username] = firstname
-		with open('cgi-bin/user_database', 'wb') as f:
+		with open('user_database', 'wb') as f:
 			pickle.dump(self, f)
 
 def printAccPage(session):
-	print("Content-type: text/html\r\n")
+	print("Content-Type: text/html\r\n")
 	print("<html>")
 	print("<head>")
 	print("<title>Account Page</title>")
@@ -37,11 +44,11 @@ def printAccPage(session):
 	print("<h1>Welcome Again", session.name, "!</h1>")
 	print("<p>Your Session ID is: ", session.getSid(), "</p>")
 	print("</body>")
-	print("<a href=\"/index.html\"> Click here to go back to homepage </a>")
+	print("<a href=\"../index.html\"> Click here to go back to homepage </a>")
 	print("</html>")
 
 def printUserMsg(msg):
-	print("Content-type: text/html\r\n")
+	print("Content-Type: text/html\r\n")
 	print("<html>")
 	print("<head>")
 	print("<title>USER MSG</title>")
@@ -49,7 +56,7 @@ def printUserMsg(msg):
 	print("<body>")
 	print("<h1>", msg ,"</h1>")
 	print("</body>")
-	print("<a href=\"/login.html\"> Click here to go back to login page </a>")
+	print("<a href=\"/account/index.html\"> Click here to go back to login page </a>")
 	print("</html>")
 
 def printLogin():
@@ -57,11 +64,11 @@ def printLogin():
 	print("<html> ")
 	print("<head>")
 	print("<meta charset=\"UTF-8\" name=\"viewport\" content=\"width=device-width, initial-scale=1\">")
-	print("<link rel=\"stylesheet\" href=\"/assets/css/accstyle.css\">")
+	print("<link rel=\"stylesheet\" href=\"../css/accstyle.css\">")
 	print("<title> Login Page </title>")
 	print("</head>")
 	print("<body>  ")
-	print("<center> <h1> Amanix Login Form </h1> </center> ")
+	print("<center> <h1> CLJ Login Form </h1> </center> ")
 	print("<form action = \"../../cgi-bin/acc.py\" method = \"GET\">")
 	print("<div class=\"container\"> ")
 	print("<label>Username : </label> ")
@@ -76,8 +83,8 @@ def printLogin():
 	print("</html>")
 
 def authUser(name, password):
-	if os.path.exists('cgi-bin/user_database'):
-		with open('cgi-bin/user_database', 'rb') as f:
+	if os.path.exists('user_database'):
+		with open('user_database', 'rb') as f:
 			database = pickle.load(f)
 			if name in database.user_pass and database.user_pass[name] == password:
 				session = Session(database.user_firstname[name])
@@ -96,32 +103,32 @@ def handleLogin():
 	elif firstname == None:
 		session = authUser(form.getvalue('username'), form.getvalue('password'))
 		if(session == None):
-			printUserMsg("Failed To Login, Username or Passowrd is wrong!")
+			printUserMsg("Failed To Login, Username or Password is wrong!")
 		else:
-			print("Correct Crenditales :D",file=sys.stderr)
+			print("Correct Crenditals :D", file=sys.stderr)
 			cookies.clear()
 			cookies["SID"] = session.getSid()
 			cookies["SID"]["expires"] = 120 # Session Expires after 2 mins
-			print("HTTP/1.1 301 OK")
+			print("HTTP/1.1 200 OK")
 			print(cookies.output())
 			print("location: acc.py")
 			print("\r\n")
 	else :
-		if os.path.exists('cgi-bin/user_database'):
-			with open('cgi-bin/user_database', 'rb') as f:
+		if os.path.exists('user_database'):
+			with open('user_database', 'rb') as f:
 				database = pickle.load(f)
 				if username in database.user_pass:
 					printUserMsg("Username is already Registerd !")
 				else:
 					database.addUser(username, password, firstname)
-					printUserMsg("Account Registerd Successfully!")
+					printUserMsg("Account Registered Successfully!")
 		else:
 			database = UserDataBase()
 			if username in database.user_pass:
 				printUserMsg("Username is already Registerd !")
 			else:
 				database.addUser(username, password, firstname)
-				printUserMsg("Account Registerd Successfully!")
+				printUserMsg("Account Registered Successfully!")
 
 form = cgi.FieldStorage()
 if 'HTTP_COOKIE' in os.environ:
@@ -130,7 +137,7 @@ if 'HTTP_COOKIE' in os.environ:
 
 	if "SID" in cookies:
 		print("Your Session ID is", cookies["SID"].value,file=sys.stderr)
-		with open('cgi-bin/sessions/session_'+ cookies["SID"].value, 'rb') as f:
+		with open('sessions/session_'+ cookies["SID"].value, 'rb') as f:
 			sess = pickle.load(f)
 		printAccPage(sess)
 	else:
